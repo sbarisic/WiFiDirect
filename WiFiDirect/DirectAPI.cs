e62ff9bee789c2834dc3d9a4bb37bbf333edf74c
Saša Barišić <cartman300@net.hr>
2017-07-01 02:51:50 +0200

    API Start

---
+++ WiFiDirect/DirectAPI.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiFiDirect {
+	public unsafe delegate void OpenSessionCallback(IntPtr SessionHandle, IntPtr Context, Guid GUIDSessionInterface, int Error, int ReasonCode);
+
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public unsafe struct MacAddress {
+		public fixed byte Address[6];
+
+		public MacAddress(byte[] Addr) {
+			if (Addr.Length != 6)
+				throw new Exception("Mac address has to be 6 bytes long");
+
+			fixed (byte* AddressPtr = Address) {
+				for (int i = 0; i < 6; i++)
+					AddressPtr[i] = Addr[i];
+			}
+		}
+
+		public MacAddress(byte A, byte B, byte C, byte D, byte E, byte F) : this(new byte[] { A, B, C, D, E, F }) {
+		}
+
+		public static readonly MacAddress Broadcast = new MacAddress(0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF);
+	}
+
+	public static unsafe class DirectAPI {
+		const string DllName = "Wlanapi";
+		const CallingConvention CConv = CallingConvention.Winapi;
+		const CharSet CSet = CharSet.Ansi;
+
+		public const int WFD_API_VERSION = 0x1;
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet)]
+		public static extern int WFDOpenHandle(int ClientVersion, out int NegotiatedVersion, out IntPtr ClientHandle);
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet)]
+		public static extern int WFDCloseHandle(IntPtr ClientHandle);
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet)]
+		public static extern int WFDStartOpenSession(IntPtr ClientHandle, ref MacAddress MacAddress, IntPtr Context, OpenSessionCallback Callback, out IntPtr SessionHandle);
+
+		public static int WFDStartOpenSession(IntPtr ClientHandle, MacAddress MacAddress, IntPtr Context, OpenSessionCallback Callback, out IntPtr SessionHandle) {
+			return WFDStartOpenSession(ClientHandle, ref MacAddress, Context, Callback, out SessionHandle);
+		}
+
+		[DllImport(DllName, CallingConvention = CConv, CharSet = CSet)]
+		public static extern int WFDCloseSession(IntPtr SessionHandle);
+	}
+}
--- WiFiDirect/Program.cs
+++ WiFiDirect/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WiFiDirect {
 	class Program {
 		static void Main(string[] args) {
+			Console.Title = "Wi-Fi Direct";
+
+			int APIVer;
+			IntPtr ClientHandle;
+			int Ret = DirectAPI.WFDOpenHandle(DirectAPI.WFD_API_VERSION, out APIVer, out ClientHandle);
+
+			IntPtr SessionHandle;
+			DirectAPI.WFDStartOpenSession(ClientHandle, MacAddress.Broadcast, IntPtr.Zero, SessionCallback, out SessionHandle);
+
+			Guid GUID = Guid.NewGuid();
+			int Len = Marshal.SizeOf(typeof(Guid));
+
+			Console.WriteLine("Done!");
+			Console.ReadLine();
+		}
+
+		static void SessionCallback(IntPtr SessionHandle, IntPtr Context, Guid GUIDSessionInterface, int Error, int ReasonCode) {
+			Console.WriteLine("Error: {1}", Error, new Win32Exception(Error).Message);
+
 		}
 	}
 }
